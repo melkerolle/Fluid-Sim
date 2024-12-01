2f9b88089103a2f3a28a0921a73deb314ac5406c
melkerolle <melkertrane@outlook.com>
2024-12-01 11:41:23 +0100

    ho

--- Assets/Scripts/Sim 2D/ParticleSpawner.cs
+++ Assets/Scripts/Sim 2D/ParticleSpawner.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using Unity.Mathematics;
+using System.Security.Cryptography;
 
 public class ParticleSpawner : MonoBehaviour
 {
@@ -27,19 +28,27 @@ public class ParticleSpawner : MonoBehaviour
             {
                 if (i >= particleCount) break;
 
+
+
                 float tx = numX <= 1 ? 0.5f : x / (numX - 1f);
                 float ty = numY <= 1 ? 0.5f : y / (numY - 1f);
 
                 float angle = (float)rng.NextDouble() * 3.14f * 2;
                 Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
                 Vector2 jitter = dir * jitterStr * ((float)rng.NextDouble() - 0.5f);
-                data.positions[i] = new Vector2((tx - 0.5f) * spawnSize.x, (ty - 0.5f) * spawnSize.y) + jitter + spawnCentre;
+                Vector2 pos = new Vector2((tx - 0.5f) * spawnSize.x, (ty - 0.5f) * spawnSize.y) + jitter + spawnCentre;
+                Vector2 dif = pos - spawnCentre;
+                if (dif.magnitude > spawnSize.x/2) continue;
+                data.positions[i] = pos;
                 data.velocities[i] = initialVelocity;
                 i++;
             }
         }
 
-        return data;
+        ParticleSpawnData newData = new ParticleSpawnData(i);
+        newData.velocities = data.velocities[0..i];
+        newData.positions = data.positions[0..i];
+        return newData;
     }
 
     public struct ParticleSpawnData
